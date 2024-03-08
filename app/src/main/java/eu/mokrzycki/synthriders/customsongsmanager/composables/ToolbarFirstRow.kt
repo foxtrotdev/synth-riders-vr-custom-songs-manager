b4b144be25910f5847ca6a657aa07ea299589dc6
Michał Mokrzycki <michal@mokrzycki.eu>
2024-03-08 23:14:10 +0100

    feat: add action to see source code

--- app/src/main/java/eu/mokrzycki/synthriders/customsongsmanager/composables/ToolbarFirstRow.kt
+++ app/src/main/java/eu/mokrzycki/synthriders/customsongsmanager/composables/ToolbarFirstRow.kt
@@ -1,5 +1,7 @@
 package eu.mokrzycki.synthriders.customsongsmanager.composables
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,15 +13,34 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import eu.mokrzycki.synthriders.customsongsmanager.R
+
+private const val GITHUB_URL = "https://github.com/foxtrotdev/synth-riders-vr-custom-songs-manager"
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun ToolbarFirstRow() {
-    val appName = LocalContext.current.getString(R.string.app_name)
+    val context = LocalContext.current
+    val appName = context.getString(R.string.app_name)
+    val sourceCodeDescription = stringResource(R.string.action_source_code)
 
     SmallTopAppBar(
         title = { Text(text = appName) },
+        actions = {
+            IconButton(onClick = {
+                val intent = Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse(GITHUB_URL)
+                }
+                context.startActivity(intent)
+            }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_github),
+                    contentDescription = sourceCodeDescription
+                )
+            }
+        },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = Color.Transparent,
             titleContentColor = Color.White,
